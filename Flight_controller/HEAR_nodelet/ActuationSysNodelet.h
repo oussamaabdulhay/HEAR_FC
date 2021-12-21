2225d148bb9ca81f04f11e54c72f974848e2900a
AhmedHumais <m.ahmedhumais@gmail.com>
2021-12-21 13:52:05 +0400

    chaged hover thrust to 0.32

--- Flight_controller/HEAR_nodelet/ActuationSysNodelet.h
+++ Flight_controller/HEAR_nodelet/ActuationSysNodelet.h
@@ -17,7 +17,7 @@ public:
 private:
     const int FREQUENCY = 200;
     const float GRAV = 9.8;
-    const float HOV_THRUST = 0.4;
+    const float HOV_THRUST = 0.32;
 
     virtual void onInit();
     
